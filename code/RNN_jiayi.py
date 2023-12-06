f36587940716b42e239d78c0f41978f2d4ab0d3e
Jiayi Zhou <143651921+JiayiZhou36@users.noreply.github.com>
2023-12-06 17:17:21 -0500

    Update RNN_jiayi.py

--- code/RNN_jiayi.py
+++ code/RNN_jiayi.py
@@ -3,53 +3,9 @@ from typing import List, Mapping, Optional, Sequence
 import numpy as np
 from numpy.typing import NDArray
 import torch
-from torch import nn
-
-# define type
-FloatArray = NDArray[np.float64]
-
-
-class RNN(nn.Module):
-    def __init__(self, embedding_size: int, hidden_size: int, output_size: int):
-        super().__init__()
-        self.embedding_size = embedding_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        MAX_VOCAB_SIZE = len(vocabulary_map)  # Update this line
-        self.embedding = nn.Embedding(MAX_VOCAB_SIZE, self.embedding_size)
-        self.rnn = nn.GRU(self.embedding_size, self.hidden_size)
-        self.fc = nn.Linear(self.hidden_size, self.output_size)
-
-    def forward(self, document: Sequence[torch.Tensor]) -> torch.Tensor:
-        embedded = self.embedding(document)
-        output, _ = self.rnn(embedded)
-        output = self.fc(output[-1, :, :])
-        return output
-
-
-def onehot(
-    vocabulary_map: Mapping[Optional[str], int], token: Optional[str]
-) -> FloatArray:
-    if len(vocabulary_map) < 2:
-        raise ValueError("Vocabulary size must be at least 2 for one-hot encoding.")
-
-    embedding = np.zeros((len(vocabulary_map),))
-    idx = vocabulary_map.get(token, len(vocabulary_map) - 1)
-    embedding[idx] = 1
-    return embedding.reshape(1, -1)
-
-
-def prepare_data(
-    documents: List[List[str]], vocabulary_map: Mapping[str, int]
-) -> Sequence[torch.Tensor]:
-    data = []
-    for sentence_list in documents:
-        sentence_data = []
-        for token in sentence_list:
-            if token in vocabulary_map:
-                sentence_data.append(vocabulary_map[token])
-        data.append(torch.tensor(sentence_data, dtype=torch.long))
-    return data
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
 
 def read_file_to_sentences(file_path):
@@ -57,54 +13,88 @@ def read_file_to_sentences(file_path):
         return [line.strip().split(",") for line in file if line.strip()]
 
 
-# Read and process files
-music = read_file_to_sentences("../data/category10.txt")
-sports = read_file_to_sentences("../data/category17.txt")
+# Define the RNN model
+class SimpleRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(SimpleRNN, self).__init__()
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.rnn = nn.RNN(hidden_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        out, _ = self.rnn(x)
+        out = self.fc(out[:, -1, :])  # Take the output from the last time step
+        return out
+
+
+# Define a custom dataset with sequence padding
+class CustomDataset(Dataset):
+    def __init__(self, data, labels, vocab):
+        self.data = data
+        self.labels = labels
+        self.vocab = vocab
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        indexed_data = [self.vocab[word] for word in self.data[idx]]
+        return torch.LongTensor(indexed_data), torch.LongTensor([self.labels[idx]])
+
+
+def pad_sequences(batch):
+    # Separate input sequences and labels
+    sequences, labels = zip(*batch)
+
+    # Pad input sequences
+    max_len = max(len(seq) for seq in sequences)
+    padded_sequences = [
+        torch.cat([seq, torch.zeros(max_len - len(seq)).long()]) for seq in sequences
+    ]
+    padded_sequences = torch.stack(padded_sequences)
+
+    # Convert labels to a tensor
+    labels = torch.LongTensor(labels)
+
+    return padded_sequences, labels
+
+
+music_data = read_file_to_sentences("../data/category10.txt")
+sports_data = read_file_to_sentences("../data/category17.txt")
 print("Read files done")
-vocabulary = sorted(set(token for sentence in music + sports for token in sentence)) + [
-    None
-]
-vocabulary_map = {token: idx for idx, token in enumerate(vocabulary)}
-print("Vocabulary done")
-MAX_VOCAB_SIZE = len(vocabulary_map)  # Define your desired maximum vocabulary size
-music_data = prepare_data(music, vocabulary_map)
-sports_data = prepare_data(sports, vocabulary_map)
-print("Prepared data done")
 
-# Create labels
-y_true = [torch.tensor([0]) for _ in music_data] + [
-    torch.tensor([1]) for _ in sports_data
-]
+all_data = music_data + sports_data
+labels = [0] * len(music_data) + [1] * len(sports_data)  # 0 for music, 1 for sports
 
-# Combine datasets
-X = music_data + sports_data
+# Flatten the list of lists
+all_data_flat = [word for sublist in all_data for word in sublist]
 
-# Define RNN model
-embedding_size = len(vocabulary_map)
-hidden_size = 128
-output_size = 2
+# Create a vocabulary for embedding
+vocab = {word: idx for idx, word in enumerate(set(all_data_flat))}
 
-model = RNN(embedding_size, hidden_size, output_size)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-loss_fn = torch.nn.CrossEntropyLoss()
+# Create the RNN model
+input_size = len(vocab)
+hidden_size = 64
+output_size = 2  # Two categories: music and sports
+model = SimpleRNN(input_size, hidden_size, output_size)
 
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Usage
-def train_rnn_model(model, X, y_true, optimizer, loss_fn, num_epochs=100):
-    total_loss = 0
-    for epoch in range(num_epochs):
-        for x_i, y_i in zip(X, y_true):
-            optimizer.zero_grad()
-            y_pred = model(x_i.unsqueeze(0))  # Adjust the input format
-            loss = loss_fn(y_pred, y_i.squeeze())
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
+# Create DataLoader for training with sequence padding
+dataset = CustomDataset(all_data, labels, vocab)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=pad_sequences)
 
-        if (epoch + 1) % 10 == 0:
-            print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
+# Training loop
+num_epochs = 10
+for epoch in range(num_epochs):
+    for inputs, labels in dataloader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels.squeeze())
+        loss.backward()
+        optimizer.step()
 
-    print("Final total loss:", total_loss)
-
-
-train_rnn_model(model, X, y_true, optimizer, loss_fn, num_epochs=100)
+    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
